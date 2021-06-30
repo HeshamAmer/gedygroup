393780f1e377302aa4fdb18d714cf4b55cf01b87
czwartaoslpoj <perygrynacyjna@gmail.com>
2021-06-30 14:28:23 +0200

    Move HomePageTitle styling to separated css file

--- src/components/HomePageTitle.js
+++ src/components/HomePageTitle.js
@@ -1,25 +1,11 @@
 import React, { Component } from 'react';
-import background from '../images/stock-image.jpg'
+import './HomePageTitle.css'
 import Carousel from './Carousel'
 class HomePageTitle extends Component {
   render() {
-    return <div style={
-      {    
-        backgroundImage: `url(${background})`,
-        backgroundSize: 'cover',
-        height: '100%',
-        backgroundRepeat: 'no-repeat',
-        textAlign: 'center'
-      }
-    } >
-      <h1 style={{
-        color: "white",
-        textAlign: "center",
-      }}>
-        <br />
-        <br />
-        Professional, <br /> Personal <br /> solutions </h1>
-      <br /><br />
+    return <div className='home-page-title' >
+      <h1 className='home-page-font'>
+        Professional Personal Solutions </h1>
       <Carousel />
     </div>
   }

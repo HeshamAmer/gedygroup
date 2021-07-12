09e6ea2fd87c1b0c14f14a60055ebb95a0866143
Hesham Amer <HeshamHamdyAmer@gmail.com>
2021-07-12 23:04:33 +0200

    Update pricing page base

--- src/pages/Pricing.js
+++ src/pages/Pricing.js
@@ -1,15 +1,36 @@
 import { Component } from 'react';
-import { Image } from 'react-bootstrap'
-import '../App.css'; 
+import { Image, Card, Button } from 'react-bootstrap'
+import '../App.css';
 class Pricing extends Component {
 
   render() {
-    return <div className='pricing-page'>
-      <h1 className="blackh1">Coming soon</h1>
-      <Image src="https://acegif.com/wp-content/uploads/loading-5.gif" fluid
-      style={{
-        objectFit:'cover',
-      }}/>
+    return <div  style={{display: "flex"}} className='pricing-page'>      
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Dedicated Team</Card.Title>
+          <Card.Text>
+          With an agile, innovative approach to product development. 
+          <br/>
+          we provide a team composed of back-and front-end developers, expert software engineers and automation testers.
+          <br/>
+          we aim to provide tailor-made technology that fits your needs.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Project based</Card.Title>
+          <Card.Text>
+          Got a project in mind? 
+          <br/>
+          We will happily help you design, architect and build it.
+          <br/>
+          We estimate the number of days required to build your features and we only charge of the actual days used, fully transparent.        
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   }
 }

7d7f2a649eb8a16dc33501d5c2ad6b2a0a90c137
Hesham Amer <heshamhamdyamer@gmail.com>
2021-07-19 22:30:36 +0200

    Add Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,26 +2,23 @@ import { Image } from 'react-bootstrap';
 import { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import img_1 from '../images/carousel1.jpg'
+import img_1 from '../images/1-Trust.png'
+import img_2 from '../images/2-Transparency.png'
+import img_3 from '../images/3-Seucrity.png'
+import img_4 from '../images/4-Teamwork.png'
+
+
 import './Carousel.css'
 class GedyCarousel extends Component {
 
   render() {
     return <div>
-      <Carousel showThumbs={false}>
-                <div>
-                    <Image className="smallImage" src={img_1} />
-                    <p className="legend">Cutting edge technology</p>
-                </div>
-                <div>
+      <Carousel showThumbs={false} autoPlay  infiniteLoop>
                 <Image className="smallImage" src={img_1} />
-                    <p className="legend">Transparent pricing</p>
-                </div>
-                <div>
-                <Image className="smallImage" src={img_1} />
-                    <p className="legend">A partner you can trust</p>
-                </div>
-            </Carousel>
+                <Image className="smallImage" src={img_2} />
+                <Image className="smallImage" src={img_3} />
+                <Image className="smallImage" src={img_4} />
+     </Carousel>
     </div>
   }
 }

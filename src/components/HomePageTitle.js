69219556ac3e06153dfd6f64604332a8464b479e
Ismael Khaled <penguin.2010@hotmail.com>
2021-08-03 23:05:10 +0200

    Separate carousel from main title

--- src/components/HomePageTitle.js
+++ src/components/HomePageTitle.js
@@ -5,13 +5,15 @@ import Carousel from './Carousel'
 
 class HomePageTitle extends Component {
   render() {
-    return <div className='home-page-title'>
-      <h1 className='home-page-font mt-5 display-2'>
-        Professional, Personal Solutions </h1>
-        <tr />
-        <h4 className='home-page-font display-4' style={{marginTop: '-50px'}}>Bring your ideas to life! </h4>
-      <GetInContactButton />
-      <Carousel/>
+    return <div >
+      <div className='home-page-title'>
+        <h1 className='home-page-font mt-5 display-2'>
+          Professional, Personal Solutions </h1>
+          <tr />
+          <h4 className='home-page-font display-4' style={{marginTop: '-50px'}}>Bring your ideas to life! </h4>
+        <GetInContactButton />
+        </div>
+        <Carousel/>
     </div>
   }
 }

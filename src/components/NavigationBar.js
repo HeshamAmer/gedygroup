6e35d0618f52830a10149ee6c9cfb22cc2e1672e
Noureldin Mohamed <noor.abdelatif@gmail.com>
2021-08-29 22:58:12 +0200

    Added Github Icon

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, Nav, Image } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 import logo from '../images/logo.png'
+import githublogo from '../images/githubLogo.png'
 import linkedInLogo from '../images/linkedin.png'
 import '../App.css'
 class NavigationBar extends Component {
@@ -34,6 +35,8 @@ class NavigationBar extends Component {
       <Nav className="ml-auto">
       <Nav.Link>        
           <Image title="LinkedIn" width="40dp" height="40dp" className="img-responsive" src={linkedInLogo} alt="linkedInLogo" onClick={()=>{window.open('https://linkedin.com/company/gedygroup')}}></Image>                
+          <Image title="Github" width="40dp" height="40dp" className="img-responsive" src={githublogo} alt="githubLogo" onClick={()=>{window.open('https://github.com/HeshamAmer/gedygroup')}}></Image>                
+
       </Nav.Link>
       </Nav>
       </Navbar.Collapse>

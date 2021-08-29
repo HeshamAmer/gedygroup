4d34abc9c1fe552bb70e61acf81261b2a06d8f89
Noureldin Mohamed <noor.abdelatif@gmail.com>
2021-08-29 17:51:30 +0200

    Github Logo and Readme file added

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, Image } from 'react-bootstrap'
 import { Link } from "react-router-dom";
+import githubLogo from '../images/githubLogo.png'
 import logo from '../images/logo.png'
 import linkedInLogo from '../images/linkedin.png'
 import '../App.css'
@@ -33,7 +34,8 @@ class NavigationBar extends Component {
       
       <Nav className="ml-auto">
       <Nav.Link>        
-          <Image title="LinkedIn" width="40dp" height="40dp" className="img-responsive" src={linkedInLogo} alt="linkedInLogo" onClick={()=>{window.open('https://linkedin.com/company/gedygroup')}}></Image>                
+          <Image title="LinkedIn" width="40dp" height="40dp" className="img-responsive" src={linkedInLogo} alt="linkedInLogo" onClick={()=>{window.open('https://linkedin.com/company/gedygroup')}}></Image>
+          <Image title="Github" width="40dp" height="40dp" className="img-responsive" src={githubLogo} alt="Github logo" onClick={()=>{window.open('https://github.com/HeshamAmer/gedygroup')}}></Image>                
       </Nav.Link>
       </Nav>
       </Navbar.Collapse>

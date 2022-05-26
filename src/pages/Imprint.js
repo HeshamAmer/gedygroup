aafab05b2ebdd4aac3190e0e0a7e3d808e414e72
Nour <noor.abdelatif@gmail.com>
2022-05-26 17:34:22 +0200

    Added Tax Number

--- src/pages/Imprint.js
+++ src/pages/Imprint.js
@@ -1,35 +1,37 @@
-import {Component} from 'react';
+import { Component } from "react";
 
 class Imprint extends Component {
-
-    render() {
-        return <div>
-            <h1>Imprint</h1>
-            <p>
-                Gedygroup LLC <br/> <br/><br/>
-                <br/>
-                <br/>
-                <br/>
-                Plot 2256 Building 9, Tasreeh El Amal street El Merag al Olwy <br/>
-                Cairo<br/>
-                Egypt<br/>
-
-                قطعة ٢٢٥٦ عمارة ٩ شارع تصريح العمل – المعراج العلوي – القاهرة<br/>
-            </p>
-
-            <p>
-                <br/>
-                Phone: +49 17677550698 <br/>
-                E-Mail: contact@gedygroup.com <br/>
-                Managing directors: Hesham Amer, Mohamed Amer
-            </p>
-            Registration Number: 185960 <br/>
-            Gedygroup is a limited liability company, registered under national Egyptian labor law for year 1981
-            <br/>
-
-        </div>
-
-    }
+  render() {
+    return (
+      <div>
+        <h1>Imprint</h1>
+        <p>
+          Gedygroup LLC <br /> <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          Plot 2256 Building 9, Tasreeh El Amal street El Merag al Olwy <br />
+          Cairo
+          <br />
+          Egypt
+          <br />
+          قطعة ٢٢٥٦ عمارة ٩ شارع تصريح العمل – المعراج العلوي – القاهرة
+          <br />
+        </p>
+        <p>
+          <br />
+          Phone: +49 17677550698 <br />
+          E-Mail: contact@gedygroup.com <br />
+          Managing directors: Hesham Amer, Mohamed Amer
+        </p>
+        Registration Number: 185960 Tax number: 684-523-434 <br />
+        Gedygroup is a limited liability company, registered under national
+        Egyptian labor law for year 1981
+        <br />
+      </div>
+    );
+  }
 }
 
-export default Imprint
+export default Imprint;
